38e85a04c4e4a8c0f81872a263f0a57260851ece
Christoph Seidel <c.seidel1234@gmail.com>
2023-05-30 12:09:27 +0200

    test functions added

--- morphing.py
+++ morphing.py
@@ -2,9 +2,20 @@ import numpy as np
 import math
 
 
-
 def rectangle(t,x_spec,y_spec,p):
-    if t <= x_spec - p or t >= x_spec + p:
+    """
+    Constructs a rectangular function that takes the value y_spec on the interval (x_spec - p,x_spec + p] and is zero otherwise.
+
+    Args:
+        t (float): parameter
+        x_spec (float): position where the spectrum is not zero.
+        y_spec (float): spectrum value.
+        p (float): distance from x_spec.
+
+    Returns:
+        int: rectangle function.
+    """
+    if t <= x_spec - p or t > x_spec + p:
         return 0.0
     else:
         return y_spec
@@ -17,13 +28,16 @@ class Rectangle_Function:
         self.x = x
         self.y = y
         self.domain = domain
-        vfunc = np.vectorize(rectangle)
-        self.rectangle = vfunc(domain, x, y, p)
+        self.rectangle = None
+        if domain.size > 0:
+            vfunc = np.vectorize(rectangle)
+            self.rectangle = vfunc(domain, x, y, p)
+                
 
     def to_string(self):
         return f"Rectangle_Function\n={self.rectangle}"
 
-    
+  
 
 class Global_Function:
 
@@ -36,6 +50,8 @@ class Global_Function:
         self.max_idx = np.argmax(y_spec)
         self.x_value =  x_spec[self.max_idx]
         self.func = np.zeros(domain.size)
+    
+
 
     #def to_string(self):
     #    return f"Global_Function\np={self.p}\nx_spec={self.x_spec}\ny_spec={self.y_spec}\ndomain={self.domain}\n" \
@@ -50,30 +66,18 @@ class Global_Function:
         right_gf = None
 
         if self.max_idx != 0:
-            print(' ')
-            print('left')
             left_x_spec = self.x_spec[:self.max_idx]
-            #print("left_x_spec " + str(left_x_spec))
             left_y_spec = self.y_spec[:self.max_idx]
             left_domain = self.domain <= self.x_value - self.p
-            
             left_domain = self.domain[left_domain]
             left_gf = Global_Function(self.p,left_x_spec,left_y_spec,left_domain)
-            print(left_gf.to_string())
+            
         if self.max_idx != self.x_spec.size-1:
-            print(' ')
-            print('right')
-            #print(self.to_string())
-            #print('self.max_idx = '+str(self.max_idx))
-            #print('self.x_spec.size-1 = '+str(self.x_spec.size))
-            #print("parent spec " + str(x_spec))
             right_x_spec = self.x_spec[self.max_idx+1:]
-            #print("right_x_spec " + str(right_x_spec))
             right_y_spec = self.y_spec[self.max_idx+1:]
-            right_domain = self.domain > self.x_value + self.p
+            right_domain = self.domain > self.x_value + self.p 
             right_domain = self.domain[right_domain]
             right_gf = Global_Function(self.p,right_x_spec,right_y_spec,right_domain)
-            print(right_gf.to_string())
             
         return left_gf, right_gf
 
@@ -82,23 +86,23 @@ class Global_Function:
         func = self.func
         other_func = other.func
         other_domain = other.domain
-        func[other_domain] = other_func
+        
+        if other_domain.size > 0 and self.domain.size > 0:
+            first_idx = self.domain[0]
+            shifted_other_domain = other_domain -first_idx
+            func[shifted_other_domain] = other_func
         self.func = func
 
 
 
-
-
-
 def set_global_function(gf):
+    """
+    This is a recursive method that actually has the structure of a binary tree and sets the global rectangle function to the object gf.
+    """
     if gf is not None:
-        
         func = Rectangle_Function(gf.p, gf.x_value, gf.max_spec, gf.domain)
-        print(func.to_string())
         gf.func = func.rectangle
         left_gf, right_gf = gf.split_at_max_value()
-        
-
         set_global_function(left_gf)
         set_global_function(right_gf)
         if left_gf is not None:
@@ -106,25 +110,49 @@ def set_global_function(gf):
         if right_gf is not None:
             gf.add_function(right_gf)
 
-        #return func
                 
 
-
   #### TEST
-p = 10.
-x_spec = np.array([9.,12.,50.,66.])
-y_spec = np.array([10.,34.,50.,30.])
-domain = np.arange(99)
 
-gf = Global_Function(p, x_spec, y_spec, domain)
-#print(gf.to_string())
-left_gf, right_gf = gf.split_at_max_value()
-#print(left_gf.to_string())
-#print(right_gf.to_string())
-count = 11
-set_global_function(gf)
-print(" ")
-print(gf.to_string())
+def test_Rectangle_Function():
+    p = 2.
+    x = 6.
+    y = 4.
+    domain = np.arange(11)
+    rf = Rectangle_Function(p,x,y,domain)
+    result = rf.rectangle
+    #print(result)
+    expected_result = np.array([0.,0.,0.,0.,0.,4.,4.,4.,4.,0.,0.])
+    assert np.allclose(result, expected_result), "Rectangle function do not match the expected values."
+    x = -1.
+    rf = Rectangle_Function(p,x,y,domain)
+    result = rf.rectangle
+    expected_result = np.array([4.,4.,0.,0.,0.,0.,0.,0.,0.,0.,0.])
+    #print(rf.rectangle)
+    assert np.allclose(result, expected_result), "Rectangle function do not match the expected values."
+
+test_Rectangle_Function()
+
+
+def test_Global_Function():
+    p = 2.
+    x_spec = np.array([1.,4.,7.,9.])
+    y_spec = np.array([1.,8.,7.,10.])
+    domain = np.arange(11)
+    gf = Global_Function(p,x_spec,y_spec,domain)
+    set_global_function(gf)
+    result = gf.func
+    expected_result = np.array([1.,1.,1.,8.,8.,8.,8.,7.,10.,10.,10.])
+    assert np.allclose(result, expected_result), "Global rectangle function do not match the expected values."
+    p = 10.
+    gf = Global_Function(p,x_spec,y_spec,domain)
+    set_global_function(gf)
+    result = gf.func
+    expected_result = np.array([10.,10.,10.,10.,10.,10.,10.,10.,10.,10.,10.])
+    assert np.allclose(result, expected_result), "Global rectangle function do not match the expected values."
+
+test_Global_Function()
+
 
     
         

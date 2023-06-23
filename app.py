39199b3b05736f726c78b24accb6926d2aaa00f8
Christoph Seidel <c.seidel1234@gmail.com>
2023-06-23 18:36:45 +0200

    hit buuton bug fixed

--- app.py
+++ app.py
@@ -2,18 +2,30 @@ import streamlit as st
 import numpy as np
 from initial_conditions import provide_initial_condition
 import make_sound as ms
+import make_sound_1 as ms1
 import os
 import matplotlib.pyplot as plt
 import thinkdsp
 import soundfile as sf
 import scipy.signal as signal
+from scipy.io import wavfile
 import morphing
 import math
+import surface as surf
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
+# Write a title
+st.title('Sound of Surfaces')
+st.sidebar.markdown("Choose options here:")
 
-def update_graphics():
-    p = st.session_state.p_slider
+#def update_morphing():
+#    p = st.session_state.p_slider
+
+def update_surfce():
+    surface = st.session_state.surface_box
+
+def update_random_vertices():
+    random_vertices = st.session_state.rand_vtx
 
 
 def sound_to_file():
@@ -22,94 +34,80 @@ def sound_to_file():
     waveform = st.session_state.wave_box
     p_o_b = st.session_state.pick_or_beat
     number_of_eigen_frequencies = st.session_state.noef
+    peak_range = st.session_state.peak_range_slider
+    p = st.session_state.p_slider
+    random_vertices = st.session_state.rand_vtx
     #MS.write_sound_to_file(c, number_of_eigen_frequencies, initial_func=waveform, pick_or_beat=p_o_b)
-    
 
-# Write a title
-st.title('Sound of Surfaces')
-st.sidebar.markdown("Choose options here:")
-
-surface = st.sidebar.selectbox('Select Surface', ['Power Ellipsoid', 'Minor Ellipsoid','Major Ellipsoid'], key='surface_box', on_change=sound_to_file)
-MS = ms.Make_Sound(surface)
-
-number_of_eigen_frequencies = st.sidebar.slider('Number of Overtones', 0, 100, step=1, value=20, key='noef')
-
-waveform = st.sidebar.selectbox('Select Waveform', ['cone', 'sawtooth', 'rectangle', 'cylinder'], key='wave_box', on_change=sound_to_file)
-
-p_o_b = st.sidebar.radio('Pick or Strike', ['pick', 'strike'], key='pick_or_beat')
-
+random_vertices = st.sidebar.checkbox('Vertices are randomly choosen',key='rand_vtx',on_change=sound_to_file)
+surface = st.sidebar.selectbox('Select Surface', ['Major Ellipsoid', 'Minor Ellipsoid','Power Ellipsoid'], key='surface_box', on_change=update_surfce)
+number_of_eigen_frequencies = st.sidebar.slider('Number of Overtones', 0, 100, step=1, value=20, key='noef',on_change=sound_to_file)
+waveform = st.sidebar.selectbox('Select Waveform', ['cone', 'sawtooth', 'single point', 'cylinder'], key='wave_box', on_change=sound_to_file)
+p_o_b = st.sidebar.radio('Pick or Hit', ['pick', 'hit'], key='pick_or_beat', on_change=sound_to_file)
 c = st.sidebar.slider('Propagation Velocity', 0.0, 0.6,step=0.01, value=0.1, key='c_slider', on_change=sound_to_file)
-#print(c)
+peak_range = st.sidebar.slider('Peak Range', 0.0, 1.,step=0.01, value=0.0, key='peak_range_slider', on_change=sound_to_file)
 
-wave_surface = MS.write_sound_to_file(c, number_of_eigen_frequencies, initial_func='sawtooth', pick_or_beat='pick')
 
+S = surf.Surface(surface)
+S.set_initial_idxs(random_vertices)
+initial_indices = S.initial_idxs
+MS = ms1.Make_Sound(S, initial_indices,p_o_b)
+wave_surface = MS.write_sound_to_file(c,peak_range, number_of_eigen_frequencies, waveform, p_o_b)
 
 # Get the current user's home directory
 home = os.path.expanduser("~")
-
 # Set the path to the desktop
 desktop = os.path.join(home, "Desktop")
-
 # Set the full path to the file
 filename = os.path.join(desktop, "new_signal.wav")
 
 if not os.path.exists(filename):
     os.mknod(filename)
 audio_file = open(filename, 'rb')
-
 audio_bytes = audio_file.read()
 
 st.markdown("The pure surface sound")
 st.audio(audio_bytes, format='../audio/wav')
 
 hf = MS.get_highest_frequency(c, number_of_eigen_frequencies)
-spectrum = wave_surface.make_spectrum(full=False)
-
-fig, ax = plt.subplots()
-x = spectrum.fs
-y = spectrum.hs
-#y = np.fft.fft(wave_surface.ys)
-y = np.abs(y)
+spectrum = wave_surface.make_spectrum(full=True)
+## first picture
+fig1, ax = plt.subplots()
+x1 = spectrum.fs
+y1 = spectrum.hs
+y1 = np.abs(y1)
 ax.set_title('Spectrum after Fourier-transform', fontstyle='italic')
 ax.set_xlim([0, hf * 1.2])
-ax.plot(x,y)
-st.pyplot(fig)
+ax.plot(x1,y1)
+st.pyplot(fig1)
+
 #transform 
 n = len(wave_surface.ys)
 d = 1 / wave_surface.framerate
 spectrum_domain = np.fft.fftfreq(n,d)
 framerate = wave_surface.framerate
-x_spec , y_spec = MS.get_spectrum( c, number_of_eigen_frequencies, initial_func='sawtooth', pick_or_beat='pick')
+x_spec , y_spec = MS.get_spectrum(c,peak_range, number_of_eigen_frequencies, waveform, p_o_b)
 
-
-
-    
 log_length = math.log(float(n))
-
-
-p = st.sidebar.slider('p', 0.1, log_length, step=0.01, value=0.01, key='p_slider', on_change=update_graphics)
+p = st.sidebar.slider('morphing width', 0.1, log_length, step=0.01, value=0.01, key='p_slider', on_change=sound_to_file)
 p = math.exp(p)
 
-
+x_spec , y_spec = MS.get_spectrum(c,peak_range, number_of_eigen_frequencies, waveform, p_o_b)
 Spectrum = morphing.make_spectrum(x_spec, y_spec, spectrum_domain,framerate)
 
-fig, ax = plt.subplots()
-ax.set_title('Spectrum and Morphing Function', fontstyle='italic')
-x = Spectrum.fs
-y = Spectrum.hs
-#gf = morphing.Global_Function(p, x_spec, y_spec, spectrum_domain)
-#morphing.set_global_function(gf)
-#global_func = gf.func
-#length = len(x)
-#convolution = morphing.smooth_func(global_func, p, length)
+fig2, ax2 = plt.subplots()
+ax2.set_title('Spectrum and Morphing Function', fontstyle='italic')
+x2 = Spectrum.fs
+y2 = Spectrum.hs
 convolution = MS.create_morphing_func(c, number_of_eigen_frequencies, wave_surface, p, waveform, p_o_b)
 
-ax.set_xlim([0, hf * 1.2])
-ax.plot(x,y,x,convolution)
+ax2.set_xlim([0, hf * 1.2])
+ax2.plot(x2,y2,x2,convolution)
 
 #st.write('x = ' + str(x))
 #st.write('y = ' + str(y))
-st.pyplot(fig)
+st.pyplot(fig2)
+st.checkbox('random vertices')
 
 # Get the directory where the app.py file is located
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -119,6 +117,10 @@ os.chdir(BASE_DIR)
 
 #st.markdown("Upload an audio file here:")
 #default
+
+def update_file():
+    uploaded_file = st.session_state.file
+
 cwd = os.getcwd()
 file_path = os.path.join(cwd, "data", "test.wav")
 
@@ -127,25 +129,40 @@ with open(file_path, "rb") as file:
 
 # Load the input WAV file
 data, sample_rate = sf.read(file_path)
-#print(len(data))
-#print(sample_rate)
 
-uploaded_file = st.file_uploader(label="Choose a file", type=[".wav"])
+uploaded_file = st.file_uploader(label="Choose a file",key='file', type=[".wav"])
+recorded_wave = thinkdsp.read_wave_with_scipy('test.wav')
+recorded_wave.unbias()
+recorded_wave.normalize()
 
-if uploaded_file is None:
-    uploaded_file = default_file
+if uploaded_file is not None:
+    # Create a directory to store the files if it doesn't exist
+    os.makedirs("recorded_sounds", exist_ok=True)
 
-#st.sidebar.markdown("Or select a sample file here:")
+    # Get the filename
+    filename = uploaded_file.name
+
+    # Specify the path to write the file
+    file_path = os.path.join("recorded_sounds", filename)
+
+    # Check if the file already exists
+    if not os.path.exists(file_path):
+        # Write the file to the specified path
+        with open(file_path, "wb") as file:
+            file.write(uploaded_file.getvalue())
+    recorded_wave = thinkdsp.read_wave_with_scipy(file_path)
+    recorded_wave.unbias()
+    recorded_wave.normalize()
+
 st.markdown("uploaded sound")
 st.audio(uploaded_file, format='../audio/wav')
 
 #st.write(type(uploaded_file))
 #st.write(uploaded_file)
-recorded_wave = thinkdsp.read_wave_with_scipy('test.wav')
-recorded_wave.unbias()
-recorded_wave.normalize()
 
-morphed_wave = MS.write_morphed_sound(c, number_of_eigen_frequencies, wave_surface,recorded_wave,p, waveform, p_o_b)
+
+morphed_wave,len_rec, len_conv, len_morph = MS.write_morphed_sound(c, number_of_eigen_frequencies, wave_surface,recorded_wave,p, waveform, p_o_b)
+st.write(f'len_rec = {len_rec}, len_conv = {len_conv}, len_morph = {len_morph}')
 
 filename = os.path.join(desktop, "morphed_signal.wav")
 
@@ -158,11 +175,24 @@ audio_bytes = audio_file.read()
 st.markdown("Morphed sound")
 st.audio(audio_bytes, format='../audio/wav')
 
-##wave = wave_surface * wave
-#wave.plot() 
-#plt.show()
-spectrum = wave_surface.make_spectrum()
+x_rand, y_rand = MS.set_random_spectrum(c, number_of_eigen_frequencies)
+Spec_rand = morphing.make_spectrum(x_rand, y_rand, spectrum_domain ,framerate)
+random_wave = MS.write_random_sound_to_file(c, number_of_eigen_frequencies)
+x_rand = Spec_rand.fs
+y_rand = Spec_rand.hs
 
+filename = os.path.join(desktop, "random_signal.wav")
 
+if not os.path.exists(filename):
+    os.mknod(filename)
+audio_file = open(filename, 'rb')
 
-st.write('p = ' + str(p))
+audio_bytes = audio_file.read()
+
+st.markdown("random sound")
+st.audio(audio_bytes, format='../audio/wav')
+
+fig3, ax = plt.subplots()
+ax.set_xlim([0, hf * 1.2])
+ax.plot(x_rand,y_rand)
+st.pyplot(fig3)
